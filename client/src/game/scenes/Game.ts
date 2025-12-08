00fe4f30fe9825fef39cabf57820ac8c3d23f4e8
Isaac Gifford <geekingreen@gmail.com>
2025-12-08 15:21:27 -0700

    include box2d

--- client/src/game/scenes/Game.ts
+++ client/src/game/scenes/Game.ts
@@ -1,18 +1,14 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
 
-    constructor ()
-    {
-        super('Game');
-    }
+  constructor() {
+    super("Game");
+  }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-    }
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
+  }
 }
