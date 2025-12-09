5cbad14d29e09a556a1eae53d2837f50a1e1f166
gabea <gabeasay@gmail.com>
2025-12-09 14:55:05 -0500

    - PhaserBox2D + PlanckJS

--- client/src/game/scenes/Game.ts
+++ client/src/game/scenes/Game.ts
@@ -1,14 +1,20 @@
-import { Scene } from "phaser";
+import {Scene} from "phaser";
+import {World} from "planck";
 
 export class Game extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera;
+    camera: Phaser.Cameras.Scene2D.Camera;
+    world: World;
 
-  constructor() {
-    super("Game");
-  }
+    constructor() {
+        super("Game");
 
-  create() {
-    this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
-  }
+        this.world = new World({
+            gravity: { x: 0, y: 0 },
+        });
+    }
+
+    create() {
+        this.camera = this.cameras.main;
+        this.camera.setBackgroundColor(0x00ff00);
+    }
 }
